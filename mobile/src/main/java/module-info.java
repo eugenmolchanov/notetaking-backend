eab49f1f3f67488118e0e09a8e627b7776a6f828
E.Molchanov <yauhenmalchanau@gmail.com>
2019-08-17 01:39:24 +0300

    mobile module info update

--- mobile/src/main/java/module-info.java
+++ mobile/src/main/java/module-info.java
@@ -4,4 +4,9 @@ module law.mobile.main {
     requires common;
     requires java.persistence;
     requires spring.boot;
+
+    opens com.notetakingplus.law.mobile to spring.core, spring.beans, spring.context;
+    opens com.notetakingplus.law.mobile.config to spring.core, spring.beans, spring.context;
+    opens com.notetakingplus.law.mobile.dao.impl to spring.core, spring.beans;
+    //TODO could declare module 'open' but let's see which modules will require law.mobile.main open
 }
